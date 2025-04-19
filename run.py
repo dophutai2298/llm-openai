3733cab2d45e3f2063df02d6db8c311ed38880db
dptai <dophutai.2298@gmail.com>
2025-04-19 10:53:07 +0700

    init

---
+++ run.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET'])
+def hello():
+    return 'Welcome to my chatbot'
+
+
+if __name__ == "__main__":
+    app.run()
